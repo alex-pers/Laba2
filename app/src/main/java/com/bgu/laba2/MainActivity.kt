6028791c29e096b4c2dea244a14f39da716535b7
Alekseevich <aalekseevich@arlo.com>
2023-03-02 18:56:01 +0100

    Initial commit

---
+++ app/src/main/java/com/bgu/laba2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bgu.laba2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        setContentView(R.layout.main_activity)
+        super.onCreate(savedInstanceState)
+    }
+}
